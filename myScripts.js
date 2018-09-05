069c205509e0ebcc0510e899db0bf98fe3e5a1b2
NateHiser <42867075+NateHiser@users.noreply.github.com>
2018-09-05 17:18:03 -0400

    header tweaks

--- myScripts.js
+++ myScripts.js
@@ -1,9 +1,1 @@
-h1.attr(color, '#5DFFEE');
-function changecolor(){
-    h1.attr('fill', pickRandom(color));
-}
-h1.on('click', changecolor);
 
-var str = new String("Demo Text");
-         document.write(str.fontcolor( "blue" ));
-         alert(str.fontcolor( "blue" ));
